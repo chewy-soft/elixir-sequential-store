d79475e4f9974eb2f040ba6e33868b483f3a7fd3
Jun Nagai <junna9a1@gmail.com>
2019-06-14 10:32:13 +0800

    [[git-now]] Fri Jun 14 10:32:13 CST 2019
     mix.exs | 2 +-
     1 file changed, 1 insertion(+), 1 deletion(-)

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule SequentialStore.MixProject do
 
   def project do
     [
-      app: :sequential_store,
+      app: :cs_sequential_store,
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
